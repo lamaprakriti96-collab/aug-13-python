55384312beea474157a5ee56182257a25f35bf44
lamaprakriti96-collab <lamaprakriti96@gmail.com>
2026-08-20 16:51:47 +0545

    update gitignore file

---
+++ day1.py
@@ -0,0 +1,33 @@
+x = 1
+y = 2
+print("hello world !", "hello class")
+
+print("hello everyone", 1, 2, 3, 4, 5, 6)
+
+print(x, y)
+
+z = 20
+
+a = 1
+A = 1
+
+data = 20
+
+DATA = 100
+
+dAtA = 1
+all_nepal_broadway = 100
+
+_data = 2
+
+data_data = 2
+
+data_ = 100
+
+date1 = 100
+
+datadata = 100
+
+# 1data=100
+# print=1
+# $data=1
